af6e662871a71631838767b8721085ab2dbed848
IOPjolnir <i.o.storozhev990@gmail.com>
2023-11-11 15:16:58 +0300

    Убрали лишнее из 4 задачи. ДОДЕЛАТЬ!!!

--- 28.10/FileName.cpp
+++ 28.10/FileName.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 
+
 //Задача 2. Палиндром.
 bool is_palindrom(std::string word) {
 	//Обеспечение нерегистрозависимости функции посредством возведения строки в нижний регистр
@@ -9,16 +10,18 @@ bool is_palindrom(std::string word) {
 	for (int i = 0; i < word.length() / 2; i++) 
 		if (word[i] != word[word.length() - i - 1]) 
 			return false;
-		}
+	return true;
+}
+
 
 //Задача 3.
 int special_chars(std::string str) {
 	//плохой способ решения
-/*	int counter = 0;
+	/* int counter = 0;
 	for (int i = 0; i < str.length(); i++)
 		if (str[i] == ' ' || str[i] == ',' || str[i] == '.' || str[i] == '?' || str[i] == '!')
 			counter++;
-	return counter;*/
+	return counter; */
 
 	//Правильный способ решения.
 	const std::string specials = " ,.!?";//строка со всеми символами, которые необходимо найти в передаваемой строке
@@ -32,17 +35,19 @@ int special_chars(std::string str) {
 	return counter;
 }
 
-//Задача 4.
-int last_char(std::string last, char n) {
-	for (int i = 0; i < last.rfind(n); i++)
-		if (last[i] == n)
-			return last[i];
+
+//Задача 4. 
+std::string last_char(std::string last, char n) {
+	last.rfind(n);
+	// TODO: функция не должна использовать цикл. Необходимо
+	// применять rfind и substr. Функция возвращает строку!
 }
 
 
 int main() {
 	system("chcp 1251>nul");
 	int n, m;
+
 
 	//Задача 1.
 	std::cout << "Задача 1. Пример.\n";
@@ -54,20 +59,23 @@ int main() {
 			res += sym;
 	std::cout << res << std::endl << std::endl;
 
+
 	//Задача 2. Палиндром.
-	std::cout << "Задача 2. \nВведите слово -> ";
+	std::cout << "Задача 2.\nВведите слово -> ";
 	std::string str2;
 	std::getline(std::cin, str2);
 	if (is_palindrom(str2))
-		std::cout << "Слово - палиндром.\n";
+		std::cout << "Слово - палиндром.\n\n";
 	else
 		std::cout << "Слово не палиндром.\n\n";
+
 
 	//Задача 3. Спец.символы.
 	std::cout << "Задача 3.\nВведите строку -> ";
 	std::string str3;
 	std::getline(std::cin, str3);
 	std::cout << "Спец.символов в строке: " << special_chars(str3) << "\n\n";
+
 
 	//Задача 4.
 	std::cout << "Задача 4.\nВведите строку ->\n";
